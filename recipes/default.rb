5296f2c0fbf6435c4b985851b56d47816437204b
root <root@ip-172-31-23-57.ap-south-1.compute.internal>
2017-12-14 08:43:43 +0000

    Initial AWS Commit

---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: aws
+# Recipe:: default
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
---
+++ recipes/setup.rb
@@ -0,0 +1,24 @@
+package 'vim-enhanced'
+package 'nano'
+
+
+package 'tree' do
+action :install
+end
+
+package 'git' do
+action :install
+end
+
+package 'ntp' 
+
+file '/etc/motd' do
+content 'This Server is the property of content by Karthick Selvam'
+owner 'root'
+group 'root'
+action :create
+end
+
+service 'ntpd' do
+action [ :enable, :start ]
+end
